cc4c16f60e24d00417f35bbb6c6647d375be09b0
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2025-07-07 18:15:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const io = new Server(server, {
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://www.aromahut.in/',
+  origin: 'https://www.aromahut.in',
 }));
 
 app.use(rateLimit({
